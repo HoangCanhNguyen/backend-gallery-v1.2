96059d4791884880774cf29372e73efb5b89c16c
HoangCanhNguyen <canh.chn@gmail.com>
2020-08-24 12:14:56 +0700

    Adding CORS

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'bkai@123'
 
 app.config['SECURITY_PASSWORD_SALT'] = 'this is super secret'
